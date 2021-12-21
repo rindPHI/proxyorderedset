26e2315d9df51dd167e70deef99cfd50eea34956
Dominic Steinhöfel <dominic.steinhoefel@cispa.de>
2021-12-21 11:53:45 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='proxyorderedset',
+    version='0.1',
+    packages=['orderedset'],
+    url='https://projects.cispa.saarland/c01dost/islearn',
+    license='GNU GPLv3',
+    author='Dominic Steinhoefel',
+    author_email='dominic.steinhoefel@cispa.de',
+    description='OrderedSet implementation as a proxy to dict',
+    install_requires=[],
+)
