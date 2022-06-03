8fda48c752efa12ee5accdfd5798fcd087d747df
Dominic Steinhöfel <dominic.steinhoefel@cispa.de>
2022-06-03 09:33:02 +0200

    Requirements cleanup, version increase

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='proxyorderedset',
-    version='0.2',
+    version='0.3',
     packages=['orderedset'],
     url='https://projects.cispa.saarland/c01dost/islearn',
     license='GNU GPLv3',
@@ -10,16 +10,15 @@ setup(
     author_email='dominic.steinhoefel@cispa.de',
     description='OrderedSet/FrozenOrderedSet implementation as a proxy to dict/frozendict',
     install_requires=[
-        "attrs>=21.2.0",
-        "iniconfig>=1.1.1",
-        "frozendict>=2.3.1",
-        "packaging>=21.0",
-        "pluggy>=0.12.0",
-        "py>=1.11.0",
-        "pyparsing>=2.4.7",
-        "pytest>=6.2.5",
+        "frozendict>=2.3.2",
+        "pytest-cov>=3.0.0",
+        "pytest-forked>=1.3.0",
         "pytest-html>=3.1.1",
-        "pytest-metadata>=1.11.0",
-        "toml>=0.10.2",
+        "pytest-profiling>=1.7.0",
+        "pytest-pycharm>=0.7.0",
+        "pytest-rerunfailures>=10.2",
+        "pytest-xdist>=2.4.0",
+        "pytest>=6.2.5",
+        "pytest>=7.1.2"
      ],
 )
